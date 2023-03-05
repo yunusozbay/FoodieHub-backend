cc52d8c7b54d223d8cd8745cef06def61bc80fa0
relentless95 <76557555+relentless95@users.noreply.github.com>
2023-03-05 16:22:05 +0100

    added the profile routes

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -3,11 +3,28 @@ const router = require("express").Router();
 const User = require("../models/User.model");
 const isAuthenticated = require("../middlewares/isAuthenticated");
 
-router.get("/:username", isAuthenticated ,async (req, res, next) => {
-  const username = req.params["username"];
-  const userProfile = await User.find({ username: username });
-  console.log(userProfile);
+router.get("/:id", isAuthenticated, async (req, res, next) => {
+  const { id } = req.params;
+  const userProfile = await User.findById(id);
   res.status(200).json({ ...userProfile });
 });
 
-module.exports = router; 
+router.put("/:id", isAuthenticated, async (req, res, next) => {
+  const { id } = req.params;
+  const updateUserProfile = req.body;
+  await User.findByIdAndUpdate(id, updateUserProfile);
+  res.json({ message: "User profile updated properly" });
+});
+
+router.delete("/:id", isAuthenticated, async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    // Delete one recipe
+    await User.findByIdAndDelete(id);
+    res.json({ message: "user profile deleted properly" });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+module.exports = router;
