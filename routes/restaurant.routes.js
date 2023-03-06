f291187112121d2103ae06360c2b4bb0352679d9
relentless95 <76557555+relentless95@users.noreply.github.com>
2023-03-06 18:17:01 +0100

    pulled the changes from the main and resolved conflicts

---
+++ routes/restaurant.routes.js
@@ -0,0 +1,58 @@
+const router = require("express").Router();
+const User = require("../models/User.model");
+const Restaurant = require("../models/Restaurant.model");
+
+router.post("/add", async (req, res, next) => {
+  const restaurant = req.body.restaurant;
+  try {
+    const resto = await Restaurant.create({
+      name: restaurant.name,
+      image_url: restaurant.image_url,
+      address: restaurant.location,
+      coordinates: restaurant.coordinates,
+      phone: restaurant.display_phone,
+      price: restaurant.price,
+      rating: restaurant.rating,
+      review_count: restaurant.review_count,
+    });
+    const userId = req.body.userData.id;
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      { $push: { restaurants: resto } },
+      { new: true }
+    );
+    console.log(updatedUser);
+    res
+      .status(201)
+      .json({ message: "Restaurant created and added to the list of user" });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.get("/:id", async (req, res, next) => {
+  try {
+    const userId = req.body.userData.id;
+    const restos = await User.findById(userId).populate("restaurants");
+    res.status(200).json(restos);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.post("/update", async (req, res, next) => {
+  const updatedRestaurant = await User.findByIdAndDelete();
+  res.status(200).json(updatedRestaurant);
+});
+
+router.post("/delete", async (req, res, next) => {
+  try {
+    const restoId = req.body.id
+    const restaurant = await Restaurant.findByIdAndDelete(restoId);
+    res.status(200).json({restaurant});
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+module.exports = router;
