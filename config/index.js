05e52cd0ddd10e964c8f680e99de7c6ce295b8fd
Yunus Ozbay <yunus.ozbay@hotmail.com>
2023-03-08 17:51:51 +0100

    export variables to env

--- config/index.js
+++ config/index.js
@@ -24,7 +24,7 @@ module.exports = (app) => {
   // controls a very specific header to pass headers from the frontend
   app.use(
     cors({
-      origin: [FRONTEND_URL]
+      origin: [FRONTEND_URL, "https://the-foodie-hub.netlify.app/" ]
     })
   );
 
